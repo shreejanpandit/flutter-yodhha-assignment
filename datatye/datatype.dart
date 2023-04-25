9407ca2fbc038921c717003ed2e6ed79943b856b
shreejanpandit <shreezanpandit@gmail.com>
2023-04-25 08:20:07 +0545

    checking datatype of variables we declare

---
+++ datatye/datatype.dart
@@ -0,0 +1,18 @@
+void main() {
+  //Declaring variables and assigning a value.
+  int myNumber = 35;
+  String myString = 'Shreejan Pandit';
+  bool myBoolean = false;
+  var myVariable = 10.01;
+  dynamic myDynamic = 'test';
+  double myDouble = 1.11;
+
+//Print the data type of the variable.
+  print(myNumber.runtimeType);
+  print(myString.runtimeType);
+  print(myBoolean.runtimeType);
+  print(myVariable.runtimeType);
+  print(myNumber.runtimeType);
+  print(myDynamic.runtimeType);
+  print(myDouble.runtimeType);
+}
