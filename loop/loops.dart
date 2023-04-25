348bd543f375eb35945079081b9aa00eda9eac5a
shreejanpandit <shreezanpandit@gmail.com>
2023-04-25 10:25:20 +0545

    printing value from 1-100, continue at 29 and break at 69

--- loop/loops.dart
+++ loop/loops.dart
@@ -22,12 +22,25 @@ void main() {
     }
   }
 
-  int i = 1;
   print("Odd numbers from 0-20 using while loop");
+  int i = 1;
   while (i <= 20) {
     if (i % 2 != 0) {
       print(i);
     }
     i++;
+  }
+
+  print('====================================================================');
+  /*  Write a  loop that prints the number from 1 to 100 loop, 
+  breaks if the number is 69 and continues if the number is 29. */
+
+  for (var i = 0; i < 100; i++) {
+    if (i == 29) {
+      continue;
+    } else if (i == 69) {
+      break;
+    }
+    print(i);
   }
 }
