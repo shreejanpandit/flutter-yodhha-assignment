5289664b59d36562315204598be4cbfca6d00305
shreejanpandit <shreezanpandit@gmail.com>
2023-04-25 09:55:59 +0545

    switch case to print gradeLetter according to provided gradeMarks

--- switch_case/switch_case.dart
+++ switch_case/switch_case.dart
@@ -31,4 +31,68 @@ prints the corresponding day of the week (e.g. "Monday", "Tuesday", etc.) for va
     default:
       print("Invalid day");
   }
+
+/*Q2. Write a switch statement that checks a variable fruit and 
+prints the corresponding color of the fruit (e.g. "apple" is red, "banana" is yellow, etc.).
+ For any other value, print "Unknown fruit".      */
+
+  String fruit = 'apple';
+  switch (fruit) {
+    case 'apple':
+      print("Apple is Red");
+      break;
+    case 'banana':
+      print("Banana is Yellow");
+      break;
+    default:
+      print("Unknown fruit");
+  }
+
+/*Q3. Write a switch statement that checks a variable language and
+ prints the corresponding greeting (e.g. "Hello" in English, "Bonjour" in French, etc.) 
+ for values of "English", "French", "Spanish", and "German".
+ For any other value, print "Unknown language".      */
+
+  String language = 'Spanish';
+  switch (language) {
+    case 'English':
+      print('Hello');
+      break;
+    case 'French':
+      print('Bonjour');
+      break;
+    case 'Spanish':
+      print('Hola');
+      break;
+    case 'German':
+      print('Hallo');
+      break;
+    default:
+      print('Unknown Language');
+  }
+
+/*Q4. Write a switch statement that checks a variable grade and 
+prints the corresponding letter grade for values from 0 to 100.
+ Use the following scale: A for values from 90 to 100, B for values from 80 to 89,
+ C for values from 70 to 79, D for values from 60 to 69, and F for any other value.      */
+  // print(85 ~/ 10);
+  int grade = 58;
+  switch ((grade / 10).floor()) {
+    //both same -->  switch(grade~/10)
+    case 9:
+    case 10:
+      print('A');
+      break;
+    case 8:
+      print('B');
+      break;
+    case 7:
+      print('C');
+      break;
+    case 6:
+      print('D');
+      break;
+    default:
+      print('F');
+  }
 }
