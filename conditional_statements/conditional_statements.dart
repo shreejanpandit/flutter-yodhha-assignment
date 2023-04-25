bdb6fe1125857f7e47809000819f5e1dac4955a9
shreejanpandit <shreezanpandit@gmail.com>
2023-04-25 08:27:45 +0545

    if statement that checks if a variable age is greater than or equal to 18

---
+++ conditional_statements/conditional_statements.dart
@@ -0,0 +1,10 @@
+/*   Q1. Write an if statement that checks if a variable age is greater than or equal to 18.
+ If it is, print "You are an adult". Otherwise, print "You are a minor".   */
+void main() {
+  int age = 24;
+  if (age >= 18) {
+    print("You are an adult");
+  } else {
+    print("You are a minor");
+  }
+}
