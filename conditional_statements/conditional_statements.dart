1c648a6a26144dbcc1f81dd8e276e6c89453aaa0
shreejanpandit <shreezanpandit@gmail.com>
2023-04-25 08:33:09 +0545

    if else to show if number is odd or even

--- conditional_statements/conditional_statements.dart
+++ conditional_statements/conditional_statements.dart
@@ -1,10 +1,28 @@
+void main() {
 /*   Q1. Write an if statement that checks if a variable age is greater than or equal to 18.
  If it is, print "You are an adult". Otherwise, print "You are a minor".   */
-void main() {
   int age = 24;
   if (age >= 18) {
     print("You are an adult");
   } else {
     print("You are a minor");
+  }
+
+/*   Q2. Write an if statement that checks if a variable temperature is greater than 100.
+ If it is, print "Danger: High temperature". Otherwise, print "Normal temperature".   */
+  double temperature = 72;
+  if (temperature >= 100) {
+    print("Danger: High temperature");
+  } else {
+    print("Normal temperature");
+  }
+
+/*   Q3. Write an if-else statement that checks if a variable number is even.
+ If it is, print "The number is even". Otherwise, print "The number is odd".   */
+  double number = 72;
+  if (number % 2 == 0) {
+    print("The number is even");
+  } else {
+    print("The number is odd");
   }
 }
