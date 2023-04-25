c7cb4525904feea11acf473e77079334f9bcd2f0
shreejanpandit <shreezanpandit@gmail.com>
2023-04-25 08:12:46 +0545

    comments added

--- variables/variables.dart
+++ variables/variables.dart
@@ -1,8 +1,14 @@
 void main() {
+  //1. Declare a variable named myNumber and assign it a value of 42.
   int myNumber = 42;
-  String myString = "Hello World";
+
+  // 2. Declare a variable named myString and assign it a value of "Hello, world!".
+  String myString = "Hello, World!";
+
+  // 3. Declare a variable named myBoolean and assign it a value of true.
   bool myBoolean = true;
 
+//Print the value of the variables.
   print(myNumber);
   print(myString);
   print(myBoolean);
