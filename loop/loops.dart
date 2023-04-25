8da26e39e5a9b586654c3d53d38c974558e6ddf8
shreejanpandit <shreezanpandit@gmail.com>
2023-04-25 10:07:45 +0545

    loop to check and print even number from 0-20

---
+++ loop/loops.dart
@@ -0,0 +1,11 @@
+void main() {
+  /* Q     */
+  /* Q1. Write a for loop that prints all the even numbers from 0 to 20.     */
+  for (var i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
+
+  /* Q     */
+}
