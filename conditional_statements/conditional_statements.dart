1dff81082d57dda815080fefc37436e75316ec57
shreejanpandit <shreezanpandit@gmail.com>
2023-04-25 08:50:11 +0545

    if else to check salary

--- conditional_statements/conditional_statements.dart
+++ conditional_statements/conditional_statements.dart
@@ -78,4 +78,13 @@ void main() {
   } else {
     print('Hello stranger');
   }
+
+/*   Q9. Write an if-else statement that checks if a variable income is greater than $50,000.
+         If it is, print "You are in a high income bracket". Otherwise, print "You are in a low income bracket".   */
+  double income = 40000;
+  if (income > 50000) {
+    print('You are in a high income bracket');
+  } else {
+    print("You are in a low income bracket");
+  }
 }
